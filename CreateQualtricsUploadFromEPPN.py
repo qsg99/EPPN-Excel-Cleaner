ae15dd8e43f6dfe6362e128b4d34c5e4db14c4ad
George Qiao <73651181+qsg99@users.noreply.github.com>
2024-11-07 15:02:53 -0500

    Add files via upload

---
+++ CreateQualtricsUploadFromEPPN.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+import pandas as pd
+import re
+import io
+import os.path
+import json
+
+check_path = "/grouperexporttest.csv"
+print(os.path.isfile(check_path))
+
+#df = pd.read_csv('/grouperexporttest.csv')
